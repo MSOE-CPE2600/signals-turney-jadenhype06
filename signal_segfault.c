e808165bac1a556eac0223f9f60569fc0db2481e
jadenhype06 <hipej@msoe.edu>
2025-11-10 20:13:45 -0600

    Implemented signal_segfault.c

--- signal_segfault.c
+++ signal_segfault.c
@@ -4,15 +4,29 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Jaden Hipe
  * 
  * Brief summary of modifications:
+ * - Implemented a signal handler for SIGSEGV
+ * - Added exit(0) into the signal handler 
  */
 
 
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+/**
+ * @brief Signal handler for SIGSEG - prints a message and exits
+ */
+void handle_signal() {
+    printf("Received a signal\n");
+    //This prevents infinite loop of receiving the SIGSEGV and rerunning the dereference statment.
+    exit(0);
+}
 
 int main (int argc, char* argv[]) {
+    //Register for signal
+    signal(SIGSEGV,handle_signal);
     // Declare a null pointer
     int* i = NULL;
 
