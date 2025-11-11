44ee9153e47d7ba745007219fcc173d8ceb53922
jadenhype06 <hipej@msoe.edu>
2025-11-10 23:04:51 -0600

    Implemented signal_sigaction.c

--- signal_sigaction.c
+++ signal_sigaction.c
@@ -3,8 +3,54 @@
 * Author: Jaden Hipe
 * Course Section: 112
 * Date: 11/10/2025
-* Compile info: make signal_sigaction.c
-* Description: 
+* Compile info: make signal_sigaction
+* Description: Uses sig action to recieve the SIGUSR1 signal from another terminal.
+* Upon successfully sending the signal to the program, it will respond with the sender's pid.
+* To stop running, send SIGINT using CTRL + C.
+* Sample output:
+* Main Terminal -
+* This PID is: 4812
+* Waiting for signal
+* Recieved signal from PID: 4709
+*
+* Secondary Terminal:
+* Inputting: kill -SIGUSR1 4812
 */ 
+
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @brief Signal handler for SIGUSR1 - prints a message and exits
+ * from man page sigaction(2)
+ */
+void signal_handler(int sig, siginfo_t *info, void *ucontext)
+{
+    printf("Recieved signal from PID: %d\n", info->si_pid);
+    
+}
+
+int main() {
+
+    // Register for the signal
+    
+
+    struct sigaction sigact;
+    sigact.sa_sigaction = signal_handler;
+    sigact.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &sigact, NULL);
+    printf("This PID is: %d\n", getpid());
+    printf("Waiting for signal\n");
+
+
+    //do nothing while waiting for alarm
+    while(1)
+    {
+
+    }
+    return 0;
+}
+
 
 
